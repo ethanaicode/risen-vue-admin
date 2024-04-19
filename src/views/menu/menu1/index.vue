d757ad41744e36a71d0e93711af6b5e82557e795
Ethan Cheng <cshengs1994@gmail.com>
2024-04-19 17:03:46 +0800

    docs: 修改注释内容

--- src/views/menu/menu1/index.vue
+++ src/views/menu/menu1/index.vue
@@ -1,14 +1,7 @@
 <template>
   <div class="app-container">
     <h4>
-      三级及其以上路由缓存功能默认关闭，需要请前往此配置文件中打开：
-      <el-link
-        type="primary"
-        href="https://github.com/un-pany/v3-admin-vite/blob/main/src/config/route.ts"
-        target="_blank"
-      >
-        src/config/route.ts
-      </el-link>
+      三级及其以上路由缓存功能默认关闭，需要请前往此配置文件中打开：src/config/route.ts
     </h4>
     <el-card header="二级路由 - menu1">
       <router-view />
